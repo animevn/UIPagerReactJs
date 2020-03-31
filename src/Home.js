bb341af0dfcc548a1e90c6bfd3b671f16394fa05
animevn <haanhth21@gmail.com>
2020-03-31 15:24:26 +0700

    ok now, enough for 9 images now

--- src/Home.js
+++ src/Home.js
@@ -3,10 +3,21 @@ import $ from "jquery";
 import "bootstrap/js/dist/tab"
 import "bootstrap/js/dist/carousel";
 
+const imgList = [
+  "/images/image1.webp", "/images/image2.webp", "/images/image3.webp", "/images/image4.webp",
+  "/images/image5.webp", "/images/image6.webp", "/images/image7.webp", "/images/image8.webp",
+  "/images/image9.webp"
+];
+
+const tabInitialMap = new Map(
+  imgList.map((value, index) => [`i${index}`, false])
+);
+
+const tabInitial = Object.fromEntries(tabInitialMap);
+
 function Home() {
 
-  const [tab, setTab] = useState({tab1: true, tab2: false, tab3: false});
-
+  const [tab, setTab] = useState({...tabInitial, i0: true});
   useEffect(() => {
     const script = document.createElement("script");
     script.type = "text/babel";
@@ -16,19 +27,7 @@ function Home() {
           $('#carousel').on('slid.bs.carousel', function (event) {
             event.preventDefault();
             const id = event.relatedTarget.id;
-            switch (id) {
-              case "pills-home":
-                setTab({tab1: true, tab2: false, tab3: false});
-                break;
-              case "pills-profile":
-                setTab({tab1: false, tab2: true, tab3: false});
-                break;
-              case "pills-contact":
-                setTab({tab1: false, tab2: false, tab3: true});
-                break;
-              default:
-                break;
-          }
+            setTab({...tabInitial, [id]:true});
         })
         }
       </script>
@@ -41,20 +40,64 @@ function Home() {
     }
   }, []);
 
-  function onHomeClick(event) {
+  function onTabClick(event) {
     event.preventDefault();
-    setTab({tab1: true, tab2: false, tab3: false});
+    const id = event.target.id.substring(1);
+    setTab({...tabInitial, [id]:true});
   }
 
-  function onProfileClick(event) {
-    event.preventDefault();
-    setTab({tab1: false, tab2: true, tab3: false});
-  }
+  const imgBox = (
+    <div className="carousel-inner">
+      {
+        imgList.map((value, index) => {
+          if (index === 0){
+            return (
+              <div className="carousel-item tab-pane active" id={`i${index}`}>
+                <img className="w-100" src={value} alt={`i${index}`}/>
+              </div>
+            )
+          }else {
+            return (
+              <div className="carousel-item tab-pane" id={`i${index}`}>
+                <img className="w-100" src={value} alt={`i${index}`}/>
+              </div>
+            )
+          }
+        })
+      }
 
-  function onContactClick(event) {
-    event.preventDefault();
-    setTab({tab1: false, tab2: false, tab3: true});
-  }
+      <a className="carousel-control-prev" href="#carousel" role="button"
+         data-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="sr-only">Previous</span>
+      </a>
+
+      <a className="carousel-control-next" href="#carousel" role="button"
+         data-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="sr-only">Next</span>
+      </a>
+
+    </div>
+  );
+
+  const tabBox = (
+    <ul className="nav nav-pills mb-3 d-flex flex-row justify-content-center">
+      {
+        imgList.map((value, index)=>{
+          return (
+            <li className="nav-item">
+              <a data-toggle="pill" href={"#i" + index} id={"#i" + index}
+                 onClick={onTabClick}
+                 className={"nav-link " + (tab[`i${index}`] ? "active" : "")}>
+                {(index + 1)}
+              </a>
+            </li>
+          )
+        })
+      }
+    </ul>
+  );
 
   return (
     <div className="container mt-3 px-0">
@@ -62,54 +105,8 @@ function Home() {
       <div id="carousel" className="carousel slide tab-content"
            data-interval="false">
 
-
-        <ul className="nav nav-pills mb-3">
-          <li className="nav-item">
-            <a className={"nav-link " + (tab.tab1 ? "active" : "")}
-               data-toggle="pill" href="#pills-home" onClick={onHomeClick}>
-              Home
-            </a>
-          </li>
-          <li className="nav-item ">
-            <a className={"nav-link " + (tab.tab2 ? "active" : "")}
-               data-toggle="pill" href="#pills-profile" onClick={onProfileClick}>
-              Profile
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className={"nav-link " + (tab.tab3 ? "active" : "")}
-               data-toggle="pill" href="#pills-contact" onClick={onContactClick}>
-              Contact
-            </a>
-          </li>
-        </ul>
-
-        <div className="carousel-inner">
-
-          <div className="carousel-item tab-pane text-justify active" id="pills-home">
-            <img src="/images/image1.webp" alt="img1"/>
-          </div>
-
-          <div className="carousel-item tab-pane text-justify" id="pills-profile">
-            <img src="/images/image2.webp" alt="img2"/>
-          </div>
-          
-          <div className="carousel-item tab-pane text-justify" id="pills-contact">
-            <img src="/images/image3.webp" alt="img3"/>
-          </div>
-
-          <a className="carousel-control-prev" href="#carousel" role="button"
-             data-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="sr-only">Previous</span>
-          </a>
-          <a className="carousel-control-next" href="#carousel" role="button"
-             data-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="sr-only">Next</span>
-          </a>
-
-        </div>
+        {tabBox}
+        {imgBox}
 
       </div>
 
